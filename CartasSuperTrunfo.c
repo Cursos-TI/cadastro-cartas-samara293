c4da2d008b7310ceea5e30a1239fc3fb8d458ede
samara293 <samaracaroline293@gmail.com>
2025-11-15 17:44:32 +0000

    terminei

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,69 +9,70 @@ int main() {
 
   char estadoA;
   char codigoA01;
-  char nomecidade01[50];
+  char nomecidade01[20];
   int populacao01;
-  float areaKm01;
+  float area01;
   float pib01;
-  int PontosTuristicos01;
+  int pontosturisticos01;
 
 
   char estadoB;
-  char codigoB02;
-  char nomeCidade02[50];
+  char codigoB02[10];
+  char nomecidade02[20];
   int populacao02;
-  float areaKm02;
+  float area02;
   float pib02;
-  int pontosTuristicos02;
+  int pontosturisticos02;
 
 
   // Área para entrada de dados
 
 printf("Cadastro da carta 01\n");
-printf("Digite o estado:\n");
-scanf(" %s", &estadoA);
+printf("Digite o estado: (apenas 1 letra, ex: A)\n");
+scanf("%c", &estadoA);
 
-printf("Digite o codigo do estado:\n");
-scanf(" %s", &codigoA01);
+printf("Digite o codigo do estado: (ex: A01)\n");
+scanf("%s", &codigoA01);
 
-printf("digite o nome da cidade:\n");
-scanf("%s, &nomeCidade01");
+printf("Digite o nome da cidade:\n");
+scanf("%s", &nomecidade01);   
 
-printf("digite a quantidade de população:\n");
-scanf("%d", &populacao01);
+printf("Digite a quantidade de população: (somente números, sem ponto ou vírgula)\n");
+scanf(" %d", &populacao01);
 
-printf("digite o tamanho da area:\n");
-scanf("%f", &areaKm01);
+printf("Digite o tamanho da área: (use ponto, não vírgula)\n");
+scanf(" %f", &area01);
 
-printf("digite o PIB:\n");
-scanf("%f", &pib01);
+printf("Digite o PIB:\n");
+scanf(" %f", &pib01);
 
-printf("Digite o numero de pontos turisticos\n");
-scanf("%d", &PontosTuristicos01);
-
+printf("Digite o numero de pontos turisticos:\n");
+scanf(" %d", &pontosturisticos01);
 
 
 printf("Cadastro da carta 02\n");
-printf("Digite o estado:\n");
+
+printf("Digite o estado (apenas 1 letra, ex: A):\n");
 scanf(" %c", &estadoB);
 
-printf("Digite o codigo do estado:\n");
-scanf(" %c", &codigoB02);
+printf("Digite o codigo do estado (ex: A01):\n");
+scanf("%s", codigoB02); 
 
-printf("digite o nome da cidade:\n");
-scanf("%s, &nomeCidade02");
+printf("Digite o nome da cidade:\n");
+scanf(" %s", nomecidade02); 
 
-printf("digite a quantidade de população:\n");
-scanf("%d", &populacao02);
+printf("Digite a quantidade de população (somente números, sem ponto ou vírgula):\n");
+scanf(" %d", &populacao02);
 
-printf("digite o tamanho da area:\n");
-scanf("%f", &areaKm02);
+printf("Digite o tamanho da área (use ponto, não vírgula):\n");
+scanf(" %f", &area02);
 
-printf("digite o PIB:\n");
-scanf("%f", &pib02);
+printf("Digite o PIB:\n");
+scanf(" %f", &pib02);
 
-printf("Digite o numero de pontos turisticos\n");
-scanf("%d", &pontosTuristicos02);
+printf("Digite o numero de pontos turisticos:\n");
+scanf(" %d", &pontosturisticos02);
+
 
   // Área para exibição dos dados da cidade
 
@@ -80,20 +81,20 @@ printf("ESTADO: %c\n", estadoA);
 printf("CODIGO: %c\n", codigoA01);
 printf("NOME DA CIDADE: %s\n", nomecidade01);
 printf("QUANTIDADE DE POPULAÇÃO: %d habitantes\n", populacao01);
-printf("TAMANHO DA AREA: %.2f\n", areaKm01);
+printf("TAMANHO DA AREA: %.2f\n", area01);
 printf("PIB: %.2f bilhões de reais\n", pib01);
-printf("NUMERO DE PONTOS TURISTICOS: %d\n", PontosTuristicos01);
+printf("NUMERO DE PONTOS TURISTICOS: %d\n", pontosturisticos01);
 
 
 
 printf("CIDADE 02\n");
-printf("ESTADO: %s\n", estadoB);
-printf("CODIGO: %s\n", codigoB02);
-printf("NOME DA CIDADE: %s\n", nomeCidade02);
+printf("ESTADO: %c\n", estadoB);
+printf("CODIGO: %c\n", codigoB02);
+printf("NOME DA CIDADE: %s\n", nomecidade02);
 printf("QUANTIDADE DE POPULAÇÃO: %d habitantes\n", populacao02);
-printf("TAMANHO DA AREA: %.2f\n", areaKm02);
+printf("TAMANHO DA AREA: %.2f\n", area02);
 printf("PIB: %.2f trilhões de reais\n", pib02);
-printf(" NUMERO DE PONTOS TURISTICOS: %d\n", pontosTuristicos02);
+printf(" NUMERO DE PONTOS TURISTICOS: %d\n", pontosturisticos02);
 
 return 0;
 
